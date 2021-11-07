bd0aa6f382aa22286469244ff4a7793774049292
EC2 Default User <ec2-user@ip-172-31-23-16.us-west-2.compute.internal>
2021-11-07 16:41:40 +0000

    Updated SAGN setting

--- Runexp_SAGN_SLE_ogbnproducts.sh
+++ Runexp_SAGN_SLE_ogbnproducts.sh
@@ -1,6 +1,6 @@
 hidden_channels=256
 mlp_layer=1
-K=5
+K=3
 pretrain_path=../../proc_data_xrt/ogbn-products/X.all.xrt-emb.npy
 data_root_dir=../../dataset
 
@@ -19,7 +19,7 @@ python -u ./src/sagn.py \
     --seed 0 \
     --num-runs 10 \
     --threshold 0.95 \
-    --epoch-setting 500 200 200 \
+    --epoch-setting 300 100 100 \
     --lr 0.001 \
     --batch-size 50000 \
     --num-hidden $hidden_channels \
@@ -29,7 +29,7 @@ python -u ./src/sagn.py \
     --input-drop 0.2 \
     --label-drop 0.5 \
     --K ${K} \
-    --label-K 9 \
+    --label-K 14 \
     --use-labels \
     --weight-decay 0 \
     --warmup-stage -1 \
